fe0f188c0f1a1710b53f495511202150cf09df67
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2022-11-10 12:44:09 -0500

    Add files via upload

---
+++ mathfunction.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author fletc
+ */
+public class mathfunction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double x = 28;
+        double y = 4;
+        
+        System.out.println("Maximum of x and y is " +Math.max(x, y));
+        
+        System.out.println("Square root of y is" + Math.sqrt(y));
+        
+        System.out.println("Power of x and y is" + Math.pow(x, y));
+        
+        System.out.println("Logarithm of x is" +Math.log(x));
+        System.out.println("Logarithm of y is" +Math.log(y));
+        
+        
+        
+        
+        
+    }
+    
+}
